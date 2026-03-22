8bbd6020c5bcf706c12346144d952c532febb19e
FakeMichau <49685661+FakeMichau@users.noreply.github.com>
2026-03-22 16:36:36 +0100

    Passthrough FSRD calls

--- OptiScaler/dllmain.cpp
+++ OptiScaler/dllmain.cpp
@@ -993,6 +993,14 @@ static void CheckWorkingMode()
                 FfxApiProxy::InitFfxDx12_FG(ffxDx12FGModule);
             }
 
+            HMODULE ffxDx12DenoiserModule = nullptr;
+            ffxDx12DenoiserModule = GetDllNameWModule(&ffxDx12DenoiserNamesW);
+            if (ffxDx12DenoiserModule != nullptr)
+            {
+                LOG_DEBUG("amd_fidelityfx_denoiser_dx12.dll already in memory");
+                FfxApiProxy::InitFfxDx12_Denoiser(ffxDx12DenoiserModule);
+            }
+
             // FFX Vulkan
             HMODULE ffxVkModule = nullptr;
             ffxVkModule = GetDllNameWModule(&ffxVkNamesW);
